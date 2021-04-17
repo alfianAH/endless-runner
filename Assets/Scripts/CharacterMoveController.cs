40e12eb07d51b7c2a969d38aeb5580515045ec12
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-04-17 20:19:16 +0800

    Add movement to main character

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")] 
+    public float moveAcceleration;
+    public float maxSpeed;
+
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocityVector = rb2D.velocity;
+        velocityVector.x = Mathf.Clamp(
+            velocityVector.x + moveAcceleration * Time.deltaTime, 
+            0f, maxSpeed);
+        
+        rb2D.velocity = velocityVector;
+    }
+}
